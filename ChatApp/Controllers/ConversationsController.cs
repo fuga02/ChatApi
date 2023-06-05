2473482994236fc71022b01a56673a230af4a642
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-06-05 00:41:19 -0700

    All done

---
+++ ChatApp/Controllers/ConversationsController.cs
@@ -0,0 +1,65 @@
+﻿
+using ChatApp.Hubs;
+using ChatApp.Services;
+using ChatData.Managers;
+using ChatData.Models;
+using IdentityData.Providers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ConversationsController : ControllerBase
+{
+    private readonly ChatManager _chatManager;
+    private readonly UserProvider _userProvider;
+    private readonly IHubContext<ConversationHub> _hubContext;
+    private readonly UserConnectionIdService _userConnectionIdService;
+    public ConversationsController(ChatManager chatManager, UserProvider userProvider, IHubContext<ConversationHub> hubContext, UserConnectionIdService userConnectionIdService)
+    {
+        _chatManager = chatManager;
+        _userProvider = userProvider;
+        _hubContext = hubContext;
+        _userConnectionIdService = userConnectionIdService;
+    }
+
+    [HttpGet]
+    public async Task<List<ConversationModel>> GetConversations()
+    {
+        return await _chatManager.GetConversations(_userProvider.UserId);
+    }
+
+    [HttpGet("{conversationId}")]
+    public async Task<List<MessageModel>> GetConversationMessages(Guid conversationId)
+    {
+        return await _chatManager.GetConversationMessages(conversationId);
+    }
+
+    [HttpPost]
+    public async Task SaveMessage(NewMessageModel model)
+    {
+        await _chatManager.SaveMessage(_userProvider.UserId, model);
+        var connectionId = _userConnectionIdService.ConnectionIds.FirstOrDefault(c => c.Item1 == model.ToUserId)?.Item2;
+        if (connectionId != null)
+        {
+            await _hubContext.Clients.Client(connectionId)
+                .SendAsync("NewMessage", model);
+        }
+
+        var connectionId1 = _userConnectionIdService.ConnectionIds.FirstOrDefault(c => c.Item1 == _userProvider.UserId)?.Item2;
+        if (connectionId1 != null)
+        {
+            await _hubContext.Clients.Client(connectionId1)
+                .SendAsync("NewMessage", new MessageModel()
+                {
+                    Text = model.Text,
+                    FromUserId = _userProvider.UserId,
+                    CreatedDate = DateTime.Now
+                });
+        }
+    }
+}
---
+++ ChatBlazor/Models/IdentityModels/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatBlazor.Models.IdentityModels;
+
+public class UserModel
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string UserName { get; set; }
+
+}
